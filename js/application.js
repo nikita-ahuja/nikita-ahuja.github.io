f6d60ccd55aa515447e03d5741946339582e0cc2
Nikita Ahuja <nahuja038@gmail.com>
2017-11-30 01:30:10 -0600

    Text rotator applied... not great with responsiveness so will hold off on merging

--- js/application.js
+++ js/application.js
@@ -18,4 +18,11 @@ $(".scroll-down").on("click" ,function(){
     }, 1000);
 });
 
+$(".rotate").textrotator({
+  animation: "dissolve", // You can pick the way it animates when rotating through words. Options are dissolve (default), fade, flip, flipUp, flipCube, flipCubeUp and spin.
+  separator: " & ", // If you don't want commas to be the separator, you can define a new separator (|, &, * etc.) by yourself using this field.
+  speed: 2500 // How many milliseconds until the next word show.
+});
+
+
 });
